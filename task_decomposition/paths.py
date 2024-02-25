d934d566f00b7ebbc062aa623a82d575203c130b
jsalfity <j.salfity@utexas.edu>
2024-02-25 15:02:25 -0600

    similarity function update, rename ground truth labeling file

--- task_decomposition/paths.py
+++ task_decomposition/paths.py
@@ -4,7 +4,7 @@ cd = os.path.dirname(os.path.realpath(__file__))
 
 DEMO_CONFIG_YAML = cd + "/scripts/demo_config.yaml"
 DATA_PATH = cd + "/data"
-DATA_GT_TXT_PATH = cd + "/data/txt/groundtruths"
+DATA_GT_TXT_PATH = lambda env_name: cd + f"/data/txt/groundtruths/{env_name}"
 DATA_RAW_TXT_PATH = cd + "/data/txt/raw"
 DATA_VIDEOS_PATH = cd + "/data/videos"
 
